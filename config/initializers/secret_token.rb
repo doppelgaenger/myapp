e31a9eb690adba6dec78005a5e1aee74596c59aa
root <root@ROR-MACLAPTOP.(none)>
2012-04-19 11:29:08 -0400

    This is the intial git commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '723bdb46706168918f349f3e3021c1e73c059e976c2d8f3b595d47848a78ea4ce9caa9bfc4a9d79d84d50ca50acb3fd0d00a778969c3914fff02c419260dd480'
